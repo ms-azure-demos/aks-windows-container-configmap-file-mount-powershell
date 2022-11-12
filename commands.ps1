e2a7c8f26f602437e1a981231ea39b7f36a67e90
Joy George Kunjikkuru <joymon@gmail.com>
2022-11-12 13:00:18 -0500

    Added K8s yaml

--- commands.ps1
+++ commands.ps1
@@ -1,8 +1,12 @@
 ########## Building & running locally ##########
-docker build -t cm-file:latest .
+docker build -t config-map-file-mounting:lts-7.2-nanoserver-1809 .
 "Build completed. Starting container"
-docker run cm-file:latest
+docker run config-map-file-mounting:lts-7.2-nanoserver-1809
 
 sleep 30
 
-docker run -v .\appsettings-mount.json:c:\app\appsettings.json cm-file:latest
+docker run -v .\appsettings-mount.json:c:\app\appsettings.json config-map-file-mounting:lts-7.2-nanoserver-1809
+
+# Replace with your registry details
+docker tag config-map-file-mounting:lts-7.2-nanoserver-1809 joymon/config-map-file-mounting:lts-7.2-nanoserver-1809
+docker push joymon/config-map-file-mounting:lts-7.2-nanoserver-1809
